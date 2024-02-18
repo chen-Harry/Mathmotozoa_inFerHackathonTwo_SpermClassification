6035cd12da333fb0a0eba65396aba33d59d5e7a4
chen-Harry <harry_chen2003@yahoo.com>
2024-02-18 17:09:27 +0000

    Add files via upload

---
+++ initialise.m
@@ -0,0 +1,9 @@
+% initialises the workspace, creating the spermTable for use in
+% classification algorithm
+% requires all_data.mat and pop_pca.m to be in the same directory
+% see README for more details
+
+linearAlphaBeta
+makeMDC
+makeTable
+makeSpermTable
